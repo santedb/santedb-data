a9dc23829944935d42d6621319b0a41450b77786
Justin Fyfe <justin@fyfesoftware.ca>
2023-04-06 13:47:15 -0400

    Fixed Firebird and SQLite patch files

--- SanteDB.Persistence.Data/SQL/FBSQL/Updates/20230227-FBSQL.sql
+++ SanteDB.Persistence.Data/SQL/FBSQL/Updates/20230227-FBSQL.sql
@@ -14,8 +14,9 @@ INSERT INTO cd_set_mem_assoc_tbl (set_id, cd_id) VALUES (char_to_uuid('4e6da567-
 insert into cd_name_tbl (cd_id, efft_vrsn_seq_id, lang_cs, val) values (char_to_uuid('4d1a5c28-deb7-411e-b75f-d524f90dfa63'), 104, 'en', 'State or Province'); --#!
 
 
+-- OPTIONAL
 INSERT INTO REL_VRFY_SYSTBL (rel_vrfy_id, rel_typ_cd_id, src_cls_cd_id, trg_cls_cd_id, err_desc) 
-SELECT uuid_generate_v1(), rel_typ_cd_id, CASE WHEN SRC_CLS_CD_ID = char_to_uuid('8CF4B0B0-84E5-4122-85FE-6AFA8240C218') THEN char_to_uuid('4d1a5c28-deb7-411e-b75f-d524f90dfa63') ELSE SRC_CLS_CD_ID END,
+SELECT gen_uuid(), rel_typ_cd_id, CASE WHEN SRC_CLS_CD_ID = char_to_uuid('8CF4B0B0-84E5-4122-85FE-6AFA8240C218') THEN char_to_uuid('4d1a5c28-deb7-411e-b75f-d524f90dfa63') ELSE SRC_CLS_CD_ID END,
 CASE WHEN TRG_CLS_CD_ID = char_to_uuid('8CF4B0B0-84E5-4122-85FE-6AFA8240C218') THEN char_to_uuid('4d1a5c28-deb7-411e-b75f-d524f90dfa63') ELSE TRG_CLS_CD_ID END, err_desc
 FROM REL_VRFY_SYSTBL
 WHERE SRC_CLS_CD_ID = char_to_uuid('8CF4B0B0-84E5-4122-85FE-6AFA8240C218') OR TRG_CLS_CD_ID = char_to_uuid('8CF4B0B0-84E5-4122-85FE-6AFA8240C218')
@@ -27,10 +28,11 @@ AND NOT EXISTS (SELECT 1 FROM REL_VRFY_SYSTBL D WHERE
 DELETE FROM REL_VRFY_SYSTBL WHERE SRC_CLS_CD_ID = char_to_uuid('8CF4B0B0-84E5-4122-85FE-6AFA8240C218') OR TRG_CLS_CD_ID = char_to_uuid('8CF4B0B0-84E5-4122-85FE-6AFA8240C218') --#!
 
 
--- FIX COUNTY OR PARISH
+-- OPTIONAL
 UPDATE ENT_VRSN_TBL SET CLS_CD_ID = char_to_uuid('6eefee7d-dff5-46d3-a6a7-171ef93879c7') WHERE CLS_CD_ID = char_to_uuid('d9489d56-ddac-4596-b5c6-8f41d73d8dc5');--#!
+-- OPTIONAL
 INSERT INTO REL_VRFY_SYSTBL (rel_vrfy_id, rel_typ_cd_id, src_cls_cd_id, trg_cls_cd_id, err_desc) 
-SELECT uuid_generate_v1(), rel_typ_cd_id, CASE WHEN SRC_CLS_CD_ID = char_to_uuid('d9489d56-ddac-4596-b5c6-8f41d73d8dc5') THEN char_to_uuid('6eefee7d-dff5-46d3-a6a7-171ef93879c7') ELSE SRC_CLS_CD_ID END,
+SELECT gen_uuid(), rel_typ_cd_id, CASE WHEN SRC_CLS_CD_ID = char_to_uuid('d9489d56-ddac-4596-b5c6-8f41d73d8dc5') THEN char_to_uuid('6eefee7d-dff5-46d3-a6a7-171ef93879c7') ELSE SRC_CLS_CD_ID END,
 CASE WHEN TRG_CLS_CD_ID = char_to_uuid('d9489d56-ddac-4596-b5c6-8f41d73d8dc5') THEN char_to_uuid('6eefee7d-dff5-46d3-a6a7-171ef93879c7') ELSE TRG_CLS_CD_ID END, err_desc
 FROM REL_VRFY_SYSTBL
 WHERE SRC_CLS_CD_ID = char_to_uuid('d9489d56-ddac-4596-b5c6-8f41d73d8dc5') OR TRG_CLS_CD_ID = char_to_uuid('d9489d56-ddac-4596-b5c6-8f41d73d8dc5')
--- SanteDB.Persistence.Data/SQL/SQLITE/20230118.sql
+++ SanteDB.Persistence.Data/SQL/SQLITE/20230118.sql
@@ -1,5 +1,5 @@
 ﻿/** 
- * <feature scope="SanteDB.Persistence.Data" id="20230105" name="Update:Cumulative Update for SQLite" applyRange="0.2.0.0-0.9.0.0" invariantName="sqlite">
+ * <feature scope="SanteDB.Persistence.Data" id="20230118" name="Update:Cumulative Update for SQLite" applyRange="0.2.0.0-0.9.0.0" invariantName="sqlite">
  *	<summary>Update:Cumulative update for SQLite</summary>
  *  <isInstalled>SELECT EXISTS (SELECT 1 FROM sqlite_master WHERE tbl_name = 'PSN_TBL' AND sql LIKE '%VIP%')</isInstalled>
  * </feature>
