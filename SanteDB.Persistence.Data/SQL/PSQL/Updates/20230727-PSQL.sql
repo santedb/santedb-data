55d96302c572aee94b0c7d37753a2e90bdf1c2a9
Justin Fyfe <justin@fyfesoftware.ca>
2023-07-28 17:37:42 -0400

    Fixed PSQL update file to use GIN indexes on ref term and concept name

--- SanteDB.Persistence.Data/SQL/PSQL/Updates/20230727-PSQL.sql
+++ SanteDB.Persistence.Data/SQL/PSQL/Updates/20230727-PSQL.sql
@@ -14,4 +14,11 @@ CREATE TABLE FD_STG_PARM_SYSTBL (
 	CONSTRAINT FK_FD_STG_PARM_STG_SYSTBL FOREIGN KEY (FD_ID) REFERENCES FD_STG_SYSTBL(FD_ID)
 );
 CREATE UNIQUE INDEX FD_STG_PARM_UQ_IDX ON FD_STG_PARM_SYSTBL(FD_ID, PARM_NAME);
+
+-- CHANGE INDEX TYPE ON NAMES
+DROP INDEX IF EXISTS ref_term_name_term_name_idx;
+CREATE INDEX ref_term_name_term_name_idx ON ref_term_name_tbl USING gin (term_name gin_trgm_ops);
+DROP INDEX IF EXISTS cd_name_val_idx ;
+CREATE INDEX cd_name_val_idx ON cd_name_tbl USING gin (val gin_trgm_ops);
+
 SELECT REG_PATCH('20230727-01'); 
