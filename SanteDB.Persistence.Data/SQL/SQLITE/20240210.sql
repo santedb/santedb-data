b46b438498de1dcb1d962c5bba056f23248b39be
Justin Fyfe <justin@fyfesoftware.ca>
2024-02-10 13:43:11 -0500

    updated to not remove the unique indexes from version sequences

--- SanteDB.Persistence.Data/SQL/SQLITE/20240210.sql
+++ SanteDB.Persistence.Data/SQL/SQLITE/20240210.sql
@@ -1,7 +1,7 @@
 /** 
- * <feature scope="SanteDB.Persistence.Data" id="20240210" name="Update:Removes unique constraint from the versioning tables for clients (allowing for sync)" applyRange="0.2.0.0-0.9.0.0" environment="Client Gateway Test" invariantName="sqlite">
+ * <feature scope="SanteDB.Persistence.Data" id="20240210" name="Update:Removes unique constraint from the versioning tables for clients (allowing for sync)" applyRange="0.2.0.0-0.9.0.0" environment="Client Gateway" invariantName="sqlite">
  *	<summary>Update:Add relationship validation for server environment</summary>
- *  <isInstalled>SELECT NOT EXISTS (SELECT 1 FROM sqlite_master WHERE name='ACT_VRSN_SEQ_UQ_IDEX')</isInstalled>
+ *  <isInstalled>SELECT NOT EXISTS (SELECT 1 FROM sqlite_master WHERE name='ACT_VRSN_SEQ_UQ_IDX')</isInstalled>
  * </feature>
  */
 DROP INDEX CD_VRSN_SEQ_ID_UQ_IDX;
