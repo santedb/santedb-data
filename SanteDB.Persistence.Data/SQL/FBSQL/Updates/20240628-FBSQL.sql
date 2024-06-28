558e9ccc253fe1c517ce59dd11b45df72b600de9
Justin Fyfe <justin@fyfesoftware.ca>
2024-06-28 11:22:25 -0400

    Fixed the FirebridSQL patch

--- SanteDB.Persistence.Data/SQL/FBSQL/Updates/20240628-FBSQL.sql
+++ SanteDB.Persistence.Data/SQL/FBSQL/Updates/20240628-FBSQL.sql
@@ -4,6 +4,7 @@
  *	<isInstalled>select ck_patch('20240628-01') from rdb$database</isInstalled>
  * </feature>
  */
+-- OPTIONAL
 DROP INDEX SEC_CER_X509_THB_IDX; --#!
 CREATE UNIQUE INDEX SEC_CER_X509_THB_AUTH_IDX ON SEC_CER_TBL COMPUTED BY (CASE WHEN OBSLT_UTC IS NULL AND CER_USE = 2 THEN X509_THB ELSE NULL END);--#!
 
