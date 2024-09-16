5137cd6e8f6b2cd76a2110d4f8642ddf2e034592
Justin Fyfe <justin@fyfesoftware.ca>
2024-09-16 10:14:23 -0400

    Updated the license headers to remove user/date of file (they were not accurate - as using file system times)

--- SanteDB.Persistence.Data/Services/AdoDataSigningCertificateManagerService.cs
+++ SanteDB.Persistence.Data/Services/AdoDataSigningCertificateManagerService.cs
@@ -15,8 +15,6 @@
  * License for the specific language governing permissions and limitations under 
  * the License.
  * 
- * User: fyfej
- * Date: 2023-7-12
  */
 using SanteDB.Core.Data.Quality;
 using SanteDB.Core.Diagnostics;
