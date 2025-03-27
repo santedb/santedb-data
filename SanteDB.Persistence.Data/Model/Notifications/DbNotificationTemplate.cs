e7d7d9d40caee062a51b22e80cbc62c279d5b369
Ryan Hull <hullr@HHSC.ca>
2025-03-27 12:37:09 -0400

    JIMS-746 Created Notifications folder and initial DbNotificationTemplate file

---
+++ SanteDB.Persistence.Data/Model/Notifications/DbNotificationTemplate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SanteDB.OrmLite.Attributes;
+
+namespace SanteDB.Persistence.Data.Model.Notifications
+{
+    /// <summary>
+    /// Represents a notification template.
+    /// </summary>
+    [Table("notification_template_tbl")]
+    public class DbNotificationTemplate : DbNonVersionedBaseData
+    {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DbNotificationTemplate"/> class.
+        /// </summary>
+        public DbNotificationTemplate()
+        {
+
+        }
+
+        /// <summary>
+        /// Gets or sets the key of the notification template.
+        /// </summary>
+        public override Guid Key { get; set; }
+    }
+}
