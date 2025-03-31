517a5fbd866e1c8a9bb4de4e68377523857fe203
Ryan Hull <hullr@HHSC.ca>
2025-03-31 12:41:34 -0400

    JIMS-746 Added foreign key mapping to DbNotificationTemplate

--- SanteDB.Persistence.Data/Model/Notifications/DbNotificationTemplate.cs
+++ SanteDB.Persistence.Data/Model/Notifications/DbNotificationTemplate.cs
@@ -1,5 +1,25 @@
-﻿using System;
+﻿/*
+ * Copyright (C) 2021 - 2025, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
+ * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
+ * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ */
+
+using System;
 using SanteDB.OrmLite.Attributes;
+using SanteDB.Persistence.Data.Model.Concepts;
 
 namespace SanteDB.Persistence.Data.Model.Notifications
 {
@@ -27,7 +47,7 @@ namespace SanteDB.Persistence.Data.Model.Notifications
         /// <summary>
         /// Gets or sets the status of the notification template.
         /// </summary>
-        [Column("sts_cd_id")]
+        [Column("sts_cd_id"), ForeignKey(typeof(DbConcept), nameof(DbConcept.Key))]
         public Guid Status { get; set; }
 
         /// <summary>
