6df71f5b1c5f3157b8f74cb81949bfd44fbdb1e0
Daria <49134543+dariaost@users.noreply.github.com>
2025-06-23 16:07:07 -0400

    Scheduled notifications (#1)
    * JIMS-746 Created Notifications folder and initial DbNotificationTemplate file
    
    * JIMS-746 Updated the data model for notification templates
    
    * JIMS-749 Created database model for notifications.
    
    * JIMS-746 Added foreign key mapping to DbNotificationTemplate
    
    * JIMS-746 Updated table and column name for DbNotificationTemplate
    
    * JIMS-757 Created the data model for Parameters
    
    * JIMS-760 Created the data model for notification template contents
    
    * JIMS-758 Created the data model for parameter values
    
    * JIMS-765 Created notification template persistence service.
    
    * Updated data models to match new table and column names
    
    * JIMS-766 Created persistence service for NotificationInstance
    
    * JIMS-744 Created pSQL script for adding template and notification tables.
    
    * Fixed issue in pSQL script where constraint foreign key was pointing to the wrong table.
    
    * Added NotificationTemplate and DbNotificationTemplate mapping to ModelMap.xml
    
    * Updated pSQL script to be an embedded resource.
    
    * Updated DbNotificationInstance and added NotificationInstance to ModelMap.xml
    
    * JIMS-742 Added SQLite script for notification templates and instances
    
    * JIMS-743 Added Firebird script to create notification instances and templates
    
    * JIMS-774 Created NotificationTemplateContentsPersistenceService
    
    * JIMS-775 Created the NotificationTemplateParameterPersistenceService
    
    * Updated data models for scheduled notifications and added NotificationInstanceParameter to ModelMap
    
    * Updated property name within DbNotificationTemplateParameter
    
    * Added SourceEntityKey property mapping for NotificationInstanceParameter in ModelMap.xml
    
    * JIMS-766 Updated NotificationInstancePersistenceService to support saving associated InstanceParameters
    
    * JIMS-758 Updated the data model for InstanceParameter to inherit from DbIdentified
    
    * JIMS-776 Created persistence service for NotificationInstanceParameter
    
    * Added SourceEntityKey property mapping for NotificationTemplateContents and NotificationTemplateParameter in ModelMap.xml
    
    * JIMS-765 Updated NotificationTemplatePersistenceService to support saving associated NotificationTemplateParameters and NotificationTemplateContents
    
    * JIMS-757 Updated the data model for DbNotificationTemplateParameter to inherit from DbIdentified
    
    * Made changes to fix errors when sending a POST request to NotificationTemplate.
    
    * JIMS-775 Updated the NotificationTemplatePerameterPersistenceService to map the parameters correctly.
    
    * Updated Notification Template persistence service to get the contents and parameters.
    
    * Updated the persistence service for NotificationInstance to retrieve and include instance parameters
    
    * JIMS-781 JIMS-782 Added PostgreSQL update script for notification state and status concepts
    
    * JIMS-781 JIMS-782 Added SQLite update script for notification state and status concepts
    
    * JIMS-781 JIMS-782 Added Firebird update script for notification state and status concepts
    
    * Fixed an issue where new instance parameters were not persisted when updating a Notification Instance
    
    * Update Notification Template to correctly remove parameters when updating a template
    
    * Updated LastSentAt property of DbNotificationInstance to be DateTimeOffset
    
    * Added NotNull attribute to appropriate data model properties
    
    * Updated DbNotificationInstance's LastSentAt property to be nullable
    
    * Replace template parameter foreign key with parameter name instead.
    
    ---------
    
    Co-authored-by: Ryan Hull <hullr@HHSC.ca>
    Co-authored-by: Jordan Webber <webberjo@hhsc.ca>
    Co-authored-by: Trevor Davis <trevor@santesuite.com>

---
+++ SanteDB.Persistence.Data/Services/Persistence/Notifications/NotificationTemplatePersistenceService.cs
@@ -0,0 +1,91 @@
+﻿/*
+ * Copyright (C) 2021 - 2024, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
+ * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
+ * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ */
+using System.Linq;
+using SanteDB.Core.Notifications;
+using SanteDB.Core.Services;
+using SanteDB.OrmLite;
+using SanteDB.Persistence.Data.Model.DataType;
+using SanteDB.Persistence.Data.Model.Notifications;
+
+namespace SanteDB.Persistence.Data.Services.Persistence.Notifications
+{
+    /// <summary>
+    /// Notification template persistence service
+    /// </summary>
+    public class NotificationTemplatePersistenceService : NonVersionedDataPersistenceService<NotificationTemplate, DbNotificationTemplate>
+    {
+        /// <summary>
+        /// Creates a new notification template persistence service
+        /// </summary>
+        public NotificationTemplatePersistenceService(IConfigurationManager configurationManager, ILocalizationService localizationService, IAdhocCacheService adhocCacheService = null, IDataCachingService dataCachingService = null, IQueryPersistenceService queryPersistence = null) : base(configurationManager, localizationService, adhocCacheService, dataCachingService, queryPersistence)
+        {
+        }
+
+        protected override NotificationTemplate DoInsertModel(DataContext context, NotificationTemplate data)
+        {
+            var retVal = base.DoInsertModel(context, data);
+
+            if (data.Contents?.Any() == true)
+            {
+                retVal.Contents = base.UpdateModelAssociations(context, retVal, data.Contents).ToList();
+            }
+
+            if (data.Parameters?.Any() == true)
+            {
+                retVal.Parameters = base.UpdateModelAssociations(context, retVal, data.Parameters).ToList();
+            }
+
+            return retVal;
+        }
+
+        protected override NotificationTemplate DoConvertToInformationModel(DataContext context, DbNotificationTemplate dbModel,
+            params object[] referenceObjects)
+        {
+            var retVal = base.DoConvertToInformationModel(context, dbModel, referenceObjects);
+
+            retVal.Contents = retVal.Contents.GetRelatedPersistenceService().Query(context, o => o.SourceEntityKey == dbModel.Key).ToList();
+            retVal.SetLoaded(o => o.Contents);
+
+            retVal.Parameters = retVal.Parameters.GetRelatedPersistenceService().Query(context, o => o.SourceEntityKey == dbModel.Key).ToList();
+            retVal.SetLoaded(o => o.Parameters);
+
+            return retVal;
+        }
+
+        /// <summary>
+        /// Perform the actual update of a model object
+        /// </summary>
+        protected override NotificationTemplate DoUpdateModel(DataContext context, NotificationTemplate data)
+        {
+            var retVal = base.DoUpdateModel(context, data);
+
+            if (data.Contents?.Any() == true)
+            {
+                retVal.Contents = base.UpdateModelAssociations(context, retVal, data.Contents).ToList();
+            }
+
+            if (data.Parameters?.Any() == true)
+            {
+                retVal.Parameters = base.UpdateModelAssociations(context, retVal, data.Parameters).ToList();
+            }
+
+            return retVal;
+        }
+    }
+}
