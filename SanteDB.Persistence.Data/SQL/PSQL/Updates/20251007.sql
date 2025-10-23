9d9328fde86bed230149780c98e6834f12e340fe
Justin Fyfe <justin@fyfesoftware.ca>
2025-10-23 11:45:06 -0400

    Updated the concept name table to be able to support 3 letter ISO639-3 codes

--- SanteDB.Persistence.Data/SQL/PSQL/Updates/20251007.sql
+++ SanteDB.Persistence.Data/SQL/PSQL/Updates/20251007.sql
@@ -1,10 +1,11 @@
 /** 
- * <feature scope="SanteDB.Persistence.Data" id="20251007-01" name="Update:20251007-01"   invariantName="npgsql" >
- *	<summary>Update: Makes values optional on observations</summary>
- *	<isInstalled>select ck_patch('20251007-01')</isInstalled>
+ * <feature scope="SanteDB.Persistence.Data" id="20251007-03" name="Update:20251007-01"   invariantName="npgsql" >
+ *	<summary>Update: Makes values optional on observations and extends the language code to three characters</summary>
+ *	<isInstalled>select ck_patch('20251007-03')</isInstalled>
  * </feature>
  */
  ALTER TABLE CD_OBS_TBL ALTER COLUMN VAL_CD_ID DROP NOT NULL;
  ALTER TABLE TXT_OBS_TBL ALTER COLUMN OBS_VAL DROP NOT NULL;
  ALTER TABLE QTY_OBS_TBL ALTER COLUMN QTY DROP NOT NULL;
- SELECT REG_PATCH('20251007-01');
+ ALTER TABLE CD_NAME_TBL ALTER COLUMN LANG_CS TYPE VARCHAR(3);
+ SELECT REG_PATCH('20251007-03');
