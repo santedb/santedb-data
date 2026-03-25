dba9ba0422c67900706dd60b96e777829ceaabbd
Justin Fyfe <justin@fyfesoftware.ca>
2026-03-25 17:42:46 -0400

    Updated the update number

--- SanteDB.Persistence.Data/SQL/FBSQL/Updates/20260324.sql
+++ SanteDB.Persistence.Data/SQL/FBSQL/Updates/20260324.sql
@@ -1,7 +1,7 @@
 /** 
- * <feature scope="SanteDB.Persistence.Data" id="20260324-01" name="Update:20260324-02"   invariantName="FirebirdSQL" >
+ * <feature scope="SanteDB.Persistence.Data" id="20260324-01" name="Update:20260324-01"   invariantName="FirebirdSQL" >
  *	<summary>Update: Add sequence to relationships on Act</summary>
- *	<isInstalled>select ck_patch('20260324-02') from rdb$database</isInstalled>
+ *	<isInstalled>select ck_patch('20260324-01') from rdb$database</isInstalled>
  * </feature>
  */
 CREATE SEQUENCE act_rel_seq;--#!
@@ -10,4 +10,4 @@ CREATE SEQUENCE act_rel_seq;--#!
 CREATE TRIGGER TG_ACT_REL_TBL_SEQ FOR ACT_REL_TBL ACTIVE BEFORE INSERT POSITION 0 AS BEGIN
 	NEW.REL_SEQ_ID = NEXT VALUE FOR act_rel_seq;
 END;--#!
- SELECT REG_PATCH('20260330-02') FROM RDB$DATABASE;
+ SELECT REG_PATCH('20260330-01') FROM RDB$DATABASE;
